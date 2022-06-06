766b6d2a06759cc93b5619a641c524e45ba16c08
OMGOMGplays <loke@jenjen.se>
2022-06-06 14:27:32 +0200

    Start of rework

--- code/Game.cs
+++ code/Game.cs
@@ -1,58 +1,24 @@
-﻿using Sandbox;
+using Sandbox;
 using System;
 using System.Linq;
 
-/// <summary>
-/// This is the heart of the gamemode. It's responsible
-/// for creating the player and stuff.
-/// </summary>
-[Library( "dods2", Title = "DoDS2" )]
-partial class DoDS2 : Game
+namespace DOD 
 {
-	[ServerCmd("switchview")]
-	public static void SwitchView()
+	public partial class DODGame : Game 
 	{
-		if ( ConsoleSystem.Caller.Pawn is not DeathmatchPlayer p ) return;
+		public DODGame() 
+		{
 
-		if ( p.Camera is not FirstPersonCamera )
-		{
-			p.Camera = new FirstPersonCamera();
-		}
-		else
-		{
-			p.Camera = new ThirdPersonCamera();
-		}
-	}
-
-	public DoDS2()
-	{
-		//
-		// Create the HUD entity. This is always broadcast to all clients
-		// and will create the UI panels clientside. It's accessible 
-		// globally via Hud.Current, so we don't need to store it.
-		//
-		if ( IsServer )
-		{
-			new DeathmatchHud();
 		}
 
-		
-	}
+		public override void ClientJoined(Client cl) 
+		{
+			base.ClientJoined(cl);
 
-	public override void PostLevelLoaded()
-	{
-		base.PostLevelLoaded();
+			var pawn = new DODPlayer();
+			pawn.Respawn();
 
-		ItemRespawn.Init();
-	}
-
-	public override void ClientJoined( Client cl )
-	{
-		base.ClientJoined( cl );
-
-		var player = new DeathmatchPlayer();
-		player.Respawn();
-
-		cl.Pawn = player;
+			cl.Pawn = pawn;
+		}
 	}
 }
